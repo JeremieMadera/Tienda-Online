4f6e7026218ac1b68f50cb6f5db7e0a4dd847a10
JeremieMadera <jeremiemadera05@gmail.com>
2025-12-08 23:11:43 -0400

    Header-Layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,18 @@ import './Header.css';
 function Header() {
   return (
     <header className="header-container">
-    <h1>Hello world</h1>
+    
+      <nav className ="header-ancords">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </nav>
+            <div className ="header-buttons">
+                <button>Sign Up</button>
+                <button>Settings</button>
+            </div>
+   
+   
     </header>);
 }
 export default Header;
