36bd30f16c31f767e0c8e20e38397d28e8ffd48d
JeremieMadera <jeremiemadera05@gmail.com>
2025-12-17 19:32:41 -0400

    more layout

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,71 @@
 import './Homepage.css';
 import { ProductCard } from '../../components/Productcard/productcard';
+import assets from '../../assets/camisa.png';
+
+
+
+
 
 function Homepage() {
+
+
   return (
     <section className="homepage">
-        <div className="homepage-content">Content card
+     
+        
+        
+        <div className="homepage-content" >
+        <button className="nav-btn right" onClick={() => moveScroll('right')}>►</button>
+        <button className="nav-btn left" onClick={() => moveScroll('left')}>◄</button>
+        
+          <div className="card">
+            <article>
+              <p>Brooch-Detail Wrap Jacket</p>
+              <p>$69.99</p>
+              <button>Buy now</button>
+            </article>
+            
+            <img src={assets} alt="Product 1"></img>
+          
+            </div>
+          <div className="card">
+            <article>
+              <p>Brooch-Detail Wrap Jacket</p>
+              <p>$69.99</p>
+              <button>Buy now</button>
+            </article>
+            <img src={assets} alt="Product 2"></img></div>
+          <div className="card">
+            <article>
+              <p>Brooch-Detail Wrap Jacket</p>
+              <p>$69.99</p>
+              <button>Buy now</button>
+            </article>
+            <img src={assets} alt="Product 3"></img></div>
+          <div className="card">
+            <article>
+              <p>Brooch-Detail Wrap Jacket</p>
+              <p>$69.99</p>
+              <button>Buy now</button>
+            </article>
+            <img src={assets} alt="Product 4"></img></div>
+          <div className="card">
+            <article>
+              <p>Brooch-Detail Wrap Jacket</p>
+              <p>$69.99</p>
+              <button>Buy now</button>
+            </article>
+            <img src={assets} alt="Product 5"></img></div>
+          <div className="card">
+            <article>
+              <p>Brooch-Detail Wrap Jacket</p>
+              <p>$89.99</p>
+              <button>Buy now</button>
+            </article>
+            <img src={assets} alt="Product 6"></img></div>
         </div>
+        
+       
         <div className="info-card"><h2>Info Card</h2></div>
         <div className ="products-container">
           <ProductCard productName="Product 1" productDescription="This is the description for Product 1." price="$19.99"/>
